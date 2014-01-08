bdcdc1b222d85353a4bff81cfcc12b551dececc4
apunktbau <a.bau@gmx.net>
2014-01-08 20:52:44 +0100

    minor refactoring

--- ModeInference/Type.hs
+++ ModeInference/Type.hs
@@ -8,11 +8,7 @@ mtypeOf :: Expression MType -> MType
 mtypeOf = \case 
   ExpVar v     -> annIdAnnotation v
   ExpCon v     -> annIdAnnotation v
-  ExpApp f _   -> resultingMType $ mtypeOf f
+  ExpApp f _   -> case mtypeOf f of
+                    MType "->" Known ts -> last ts
   ExpCase _ bs -> supremum $ map (mtypeOf . branchExpression) bs
   ExpLet _ a   -> mtypeOf a
-
-resultingMType :: MType -> MType
-resultingMType = \case
-  MType "->" Known ts -> last ts
-  mtype               -> mtype
